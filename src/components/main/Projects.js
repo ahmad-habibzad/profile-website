71ad65f701854b53dc1a859f9a1dfc5b4094c9ab
Ahmad Habibzad <masoud.habibzad@gmail.com>
2021-07-15 00:51:37 -0400

    update skills

--- src/components/main/Projects.js
+++ src/components/main/Projects.js
@@ -1,76 +1,53 @@
 import React from 'react'
 
-function Projects() {
+function Skills() {
     return (
-        <div className="section" id="projects">
-            <h3 className="section-title">Projects</h3>
-            <div className="projects-container">
-                <div className=" project">
-                    <div className="project-img_container">
-                        <a href="https://habibzad.github.io/rpsGame/" rel="noreferrer" target="_blank">
-                            <img className="project-img" src="images/project1.jpg" alt="" />
-                        </a>
+        <div className="section skills flex" id="skills">
+            <div className="skills-wrapper">
+                <h2 className="section-title">skills & tools</h2>
+                <p className="skills-para">The tools and technologies I am comfortable with:</p>
+                <div className="skill-container flex">
+                    <div className="skill">
+                        <i className="fab fa-java fa-skills"></i>
+                        <h2 style={{ marginTop: '10px' }}>Java</h2>
+                        <p >I use Java and Spring Boot to create REST APIs. I write clean and well-formatted code that is easy to read, trouble-shoot and
+                            maintain.</p>
+                        {/* <p>I write clean, well formatted, semantic HTML5 making sure it is easy to read, trouble-shoot and
+                            maintain.</p> */}
                     </div>
-                    <div className="project-title">
-                        <h3 className="project-description">Rock-Paper-Scissor</h3>
-                        <p className="project-summary">A simple Rock-Paper-Scissor game
-                        </p>
+                    <div className="skill">
+                        <i className="fab fa-react fa-skills"></i>
+                        <h2 style={{ marginTop: '10px' }}>React</h2>
+                        <p>I build user interfaces using ReactJS. Additionally, I use CSS current best practices to build responsive websites.</p>
+                        {/* <p>Using CSS current best practices such as media queries and flex box, my focus is to build pixel perfect
+                            and responsive websites.</p> */}
                     </div>
-                </div>
-                <div className="project">
-                    <div className="project-img_container">
-                        <a href="https://habibzad.github.io/gpa-calculator/" rel="noreferrer" target="_blank">
-                            <img className="project-img" src="images/project2.jpg" alt="" />
-                        </a>
-                    </div>
-                    <div className="project-title">
-                        <h3 className="project-description">GPA Calculator</h3>
-                        <p className="project-summary">Basic GPA Calculator</p>
-                    </div>
-                </div>
-                <div className="project">
-                    <div className="project-img_container">
-                        <a href="https://codepen.io/amhabibzad/full/KKMOrWQ" rel="noreferrer" target="_blank">
-                            <img className="project-img" src="images/project3.jpg" alt="" />
-                        </a>
-                    </div>
-                    <div className="project-title">
-                        <h3 className="project-description">Digital Clock</h3>
-                        <p className="project-summary">Digital Clock</p>
+                    <div className="skill">
+                        <i className="fas fa-database fa-skills"></i>
+                        <h2 style={{ marginTop: '10px' }}>MySQL</h2>
+                        <p>Together with Spring Data JPA, I use MySQL to build databases for my web applications.</p>
+                        {/* <p>In addition to using Javascript to make my website dynamic, I use JavaScript for creating small scale
+                            web applications.</p> */}
                     </div>
                 </div>
-                <div className="project">
-                    <div className="project-img_container">
-                        <a href="https://habibzad.github.io/inspirational-quotes/" rel="noreferrer" target="_blank">
-                            <img className="project-img" src="images/project4.jpg" alt="" />
-                        </a>
-                    </div>
-                    <div className="project-title">
-                        <h3 className="project-description">Random quotes</h3>
-                        <p className="project-summary">Random quotes</p>
-                    </div>
-                </div>
-                <div className=" project">
-                    <div className="project-img_container">
-                        <a href="https://habibzad.github.io/to-do-list/" rel="noreferrer" target="_blank">
-                            <img className="project-img" src="images/todo.jpg" alt="" />
-                        </a>
-                    </div>
-                    <div className="project-title">
-                        <h3 className="project-description">To-Do List</h3>
-                        <p className="project-summary">A simple To-Do List
-                        </p>
-                    </div>
-                </div>
-                <div className="project">
-                    <div className="project-img_container">
-                        <a href="https://codepen.io/amhabibzad/full/OJRNPoP" rel="noreferrer" target="_blank">
-                            <img className="project-img" src="images/project6.jpg" alt="" />
-                        </a>
-                    </div>
-                    <div className="project-title">
-                        <h3 className="project-description">Weather App</h3>
-                        <p className="project-summary">Weather App</p>
+                <div className="tools">
+                    <h3 className="sub-title">Other Skills:</h3>
+                    <p className="skills-para">The following are some other tools and technologies I use to bring my projects to life:</p>
+                    <div className=" tools-container">
+                        <i className="fab fa-html5 far-skills"></i>
+                        <p>HTML5</p><br />
+                        <i className="fab fa-css3-alt far-skills"></i>
+                        <p>CSS3</p><br />
+                        <i className="fab fa-js-square far-skills"></i>
+                        <p>JavaScript</p><br />
+                        <i className="fab fa-bootstrap far-skills"></i>
+                        <p>Bootstrap</p><br />
+                        <i class="fab fa-git-square far-skills"></i>
+                        <p>Git</p><br />
+                        <i className="fab fa-github-square far-skills"></i>
+                        <p>GitHub</p><br />
+                        <span className="iconify far-skills" data-icon="bi:terminal-fill" data-inline="false"></span>
+                        <p>Terminal</p>
                     </div>
                 </div>
             </div>
@@ -78,4 +55,4 @@ function Projects() {
     )
 }
 
-export default Projects
+export default Skills
