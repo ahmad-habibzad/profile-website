d8e77a1acb5bd474c7ef3a908ad4dac677acb055
Ahmad Habibzad <66021219+ahmad-habibzad@users.noreply.github.com>
2024-01-29 20:14:07 -0500

    Add Merit America link to home page

--- src/components/main/Home.js
+++ src/components/main/Home.js
@@ -11,12 +11,12 @@ function Home() {
                     Java Developer
                 </h2>
                 <p className="body-txt">
-                    Hi, my name is Ahmad. I am a Java developer. I currently work as a
-                    Technical Advisor for Merit America
+                    Hi, my name is Ahmad. I am a Java developer, QA Automation Engineer and Technology Enthusiast. I currently work as a
+                    Technical Advisor for <a href="https://meritamerica.org/team/ahmad-habibzad/">Merit America</a>
                 </p>
-                <p className="second-para">I'm interested in Java development and testing. Feel free to reach out — I'm excited about the opportunity to collaborate and contribute to meaningful
-                    projects.</p>
-                <br/>
+                <p>If you're working on anything related to testing, test automation, or quality assurance, I'd love to
+                    join forces. Feel free to reach out—I'm excited about the opportunity to collaborate and contribute to meaningful
+                    projects. Let's make testing excellence a shared journey!</p>
                 <a href="#contact" className="cnt-btn">
                     Get In Touch
                 </a>
